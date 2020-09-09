ef2f83b77eea3706aed351ccf4707787c2ec4e49
guguasmon <hutagoazkey@gmail.com>
2020-09-09 17:33:43 +0900

    userモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    with_options presence: true do
+    validates :name
+    validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A\S+@\S+\.\S+\z/ }
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password Include both letters and numbers' }
+  end
 end
