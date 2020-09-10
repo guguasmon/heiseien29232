27a8489c556b2d73609b9973dcdda7159fed748e
guguasmon <hutagoazkey@gmail.com>
2020-09-10 15:28:38 +0900

    visit,genderとのアソシエーション記述

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,21 @@
 class Guest < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+  belongs_to_active_hash :visit
+
   belongs_to :user
+
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'full-width characters.' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'full-width characters.' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters.' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters.' }
+    validates :gender_id
+    validates :visit1_id
+    validates :visit2_id
+    with_options numericality: { other_than: 0, message: "can't be blank" } do
+      validates :gender_id
+      validates :visit1_id
+    end
+  end
 end
