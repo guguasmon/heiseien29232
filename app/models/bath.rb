b7841226159c767b6c9096f2361e7fb75b3f2722
guguasmon <hutagoazkey@gmail.com>
2020-09-10 15:48:42 +0900

    bathモデルのアソシエーション

--- app/models/bath.rb
+++ app/models/bath.rb
@@ -1,2 +1,7 @@
 class Bath < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :bathing
+  belongs_to_active_hash :infecton
+  belongs_to_active_hash :timing
+  belongs_to :guest
 end
