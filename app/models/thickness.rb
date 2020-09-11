915951b0a1c255bb0ab002b59f8edb260f2cbf86
guguasmon <hutagoazkey@gmail.com>
2020-09-11 12:47:03 +0900

    アクティブハッシュ更新

--- app/models/thickness.rb
+++ app/models/thickness.rb
@@ -1,7 +1,8 @@
 class Thickness < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '小匙半分' },
-    { id: 2, name: '小匙一杯' }
+    { id: 1, name: 'とろみなし' },
+    { id: 2, name: '小匙半分' },
+    { id: 3, name: '小匙一杯' }
   ]
 end
--- app/models/timing.rb
+++ app/models/timing.rb
@@ -1,8 +1,9 @@
 class Timing < ActiveHash::Base
   self.data = [
-    { id: 0, name: '--' },
-    { id: 1, name: '最後' },
+    { id: 0, name: '普通' },
+    { id: 1, name: '最初' },
     { id: 2, name: '早め' },
-    { id: 3, name: '最初' }
+    { id: 3, name: '遅め' },
+    { id: 4, name: '最後' }
   ]
 end
