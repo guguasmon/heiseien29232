3e36ad734a47e18013d90177998c77f9674b0791
guguasmon <hutagoazkey@gmail.com>
2020-09-13 10:26:45 +0900

    bathモデルの単体テスト

--- spec/factories/baths.rb
+++ spec/factories/baths.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :bath do
-    
+    bathing_id              { 1 }
+    infection_id            { 1 }
+    timing_id               { 2 }
+    remark_bath             { "" }
+    association :guest
   end
 end
--- spec/models/bath_spec.rb
+++ spec/models/bath_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Bath, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @bath = FactoryBot.build(:bath)
+  end
+  describe '入浴情報の新規登録' do
+    context '入浴情報の新規登録がうまくいくとき' do
+      it 'bathing_id,infection_id,timing_idが存在すれば登録できる' do
+        expect(@bath).to be_valid
+      end
+      it 'bating_idが0（未選択）以外であれば登録できる' do
+        @bath.bathing_id = '1'
+        expect(@bath).to be_valid
+      end
+      it 'infection_idが0（未選択）以外であれば登録できる' do
+        @bath.infection_id = '1'
+        expect(@bath).to be_valid
+      end
+      it 'timing_idが0(未選択)でも登録できる' do
+        @bath.timing_id = '0'
+        expect(@bath).to be_valid
+      end
+      it 'remark_bathが空でも登録できる' do
+        @bath.remark_bath = ''
+        expect(@bath).to be_valid
+      end
+
+    end
+
+    context '入浴情報の新規登録がうまくいかないとき' do
+      it 'bathing_idが0（未選択）では登録できない' do
+        @bath.bathing_id = '0'
+        @bath.valid?
+        expect(@bath.errors.full_messages).to include("Bathing can't be blank")
+      end
+      it 'infection_idが0（未選択）では登録できない' do
+        @bath.infection_id = '0'
+        @bath.valid?
+        expect(@bath.errors.full_messages).to include("Infection can't be blank")
+      end
+    end
+  end
 end
