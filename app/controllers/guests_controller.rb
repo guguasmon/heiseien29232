9230a117467018f00ac9c57ccd8021d4f91b2d27
guguasmon <hutagoazkey@gmail.com>
2020-09-14 12:01:25 +0900

    利用者の編集機能の実装

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,4 +1,6 @@
 class GuestsController < ApplicationController
+  before_action :set_guest, only: [:show, :edit, :update]
+  before_action :move_to_index, except: :index
   def index
     if user_signed_in?
       user = User.find(current_user.id)
@@ -7,13 +9,13 @@ class GuestsController < ApplicationController
   end
 
   def new
-    @guest = GuestData.new
+    @guestdata = GuestData.new
   end
 
   def create
-    @guest = GuestData.new(guest_params)
-    if @guest.valid?
-      @guest.save
+    @guestdata = GuestData.new(guest_params)
+    if @guestdata.valid?
+      @guestdata.save
       flash[:notice] = '利用者情報を登録しました'
       redirect_to root_path
     else
@@ -21,13 +23,58 @@ class GuestsController < ApplicationController
     end
   end
 
+  def edit
+    @guestdata = GuestData.new(
+      first_name: @guest.first_name,
+      last_name: @guest.last_name,
+      first_name_kana: @guest.first_name_kana,
+      last_name_kana: @guest.last_name_kana,
+      gender_id: @guest.gender_id,
+      visit1_id: @guest.visit1_id,
+      visit2_id: @guest.visit2_id,
+      description: @guest.description,
+      bathing_id: @guest.bath.bathing_id,
+      infection_id: @guest.bath.infection_id,
+      timing_id: @guest.bath.timing_id,
+      remark_bath: @guest.bath.remark_bath,
+      drink_type_id: @guest.drink.drink_type_id,
+      warm: @guest.drink.warm,
+      thickness_id: @guest.drink.thickness_id,
+      diabetes: @guest.drink.diabetes,
+      remark_drink: @guest.drink.remark_drink
+    )
+  end
+
+  def update
+    @guestdata = GuestData.new(guest_params)
+    if @guestdata.valid?
+      @guestdata.update
+      flash[:notice] = '利用者情報を更新しました'
+      redirect_to root_path
+    else
+      render('guests/edit')
+    end
+  end
+
+  def sho
+  end
+
   private
+  def set_guest
+    @guest = Guest.find(params[:id])
+  end
 
   def guest_params
     params.require(:guest_data).permit(
       :first_name, :last_name, :first_name_kana, :last_name_kana, :gender_id, :visit1_id, :visit2_id, :description,
       :bathing_id, :infection_id, :timing_id, :remark_bath,
       :drink_type_id, :warm, :thickness_id, :diabetes, :remark_drink
-    ).merge(user_id: current_user.id)
+    ).merge(id: params[:id], user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
