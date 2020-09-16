482871abce1996e51ec41b790b8775aa7fe563dc
guguasmon <hutagoazkey@gmail.com>
2020-09-16 18:40:05 +0900

    入浴形態表の曜日別表示

--- app/controllers/baths_controller.rb
+++ app/controllers/baths_controller.rb
@@ -1,21 +1,50 @@
 class BathsController < ApplicationController
   before_action :move_to_index_bath
   before_action :set_guest, only: [:edit, :update]
+  before_action :set_day_of_the_week, only: :search
 
   def index
-    @guests = current_user.guests.includes(:bath)
     # user/男子/一般浴
-    @normal_males = Guest.bath_search(current_user.id, 1, 1)
+    @normal_males = SearchGuestsService.search_bath(current_user.id, 1, 1)
     # user/女子/一般浴
-    @normal_females = Guest.bath_search(current_user.id, 2, 1)
+    @normal_females = SearchGuestsService.search_bath(current_user.id, 2, 1)
     # user/男子/一般チェアー浴
-    @normal_chair_males = Guest.bath_search(current_user.id, 1, 2)
+    @normal_chair_males = SearchGuestsService.search_bath(current_user.id, 1, 2)
     # user/女子/一般チェアー浴
-    @normal_chair_females = Guest.bath_search(current_user.id, 2, 2)
+    @normal_chair_females = SearchGuestsService.search_bath(current_user.id, 2, 2)
     # user/男子/チェアー浴
-    @chair_males = Guest.bath_search(current_user.id, 1, 3)
+    @chair_males = SearchGuestsService.search_bath(current_user.id, 1, 3)
     # user/女子/チェアー浴
-    @chair_females = Guest.bath_search(current_user.id, 2, 3)
+    @chair_females = SearchGuestsService.search_bath(current_user.id, 2, 3)
+  end
+
+  def search
+    # user/男子/一般浴
+    @normal_males = SearchGuestsService.search_bath_day(current_user.id, 1, 1, @day)
+    # user/女子/一般浴
+    @normal_females = SearchGuestsService.search_bath_day(current_user.id, 2, 1, @day)
+    # user/男子/一般チェアー浴
+    @normal_chair_males = SearchGuestsService.search_bath_day(current_user.id, 1, 2, @day)
+    # user/女子/一般チェアー浴
+    @normal_chair_females = SearchGuestsService.search_bath_day(current_user.id, 2, 2, @day)
+    # user/男子/チェアー浴
+    @chair_males = SearchGuestsService.search_bath_day(current_user.id, 1, 3, @day)
+    # user/女子/チェアー浴
+    @chair_females = SearchGuestsService.search_bath_day(current_user.id, 2, 3, @day)
+    # 曜日情報を入力
+    day_of_the_week = ["全","月","火","水","木","金","土","日"]
+    today = @day.to_i
+    @day_of_the_week = day_of_the_week[today]
+    # 利用者数を集計
+    count = 0
+    count += @normal_males.size
+    count += @normal_females.size
+    count += @normal_chair_males.size
+    count += @normal_chair_females.size
+    count += @chair_males.size
+    count += @chair_females.size
+    @count = count
+    render action: :index
   end
 
   def edit
@@ -72,4 +101,9 @@ class BathsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def set_day_of_the_week
+    @day = params[:id]
+  end
+
 end
