0cb4dbdf904fbea172ef7bebc35520eff6253fc9
guguasmon <hutagoazkey@gmail.com>
2020-09-20 16:51:31 +0900

    commentモデルのテストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :comment do
-    
+      text              { "新規登録しました" }
+      comment_type_id            { 1 }
+      before               { '変更前' }
+      after             { '変更後' }
+      association :user
+      association :guest
   end
 end
