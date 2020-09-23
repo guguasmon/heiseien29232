13962e5991678718b7c552d872d6f4f7329d66fc
guguasmon <hutagoazkey@gmail.com>
2020-09-23 17:44:21 +0900

    first_nameの並び替えて撤廃

--- app/services/search_guests_service.rb
+++ app/services/search_guests_service.rb
@@ -11,11 +11,11 @@ class SearchGuestsService
 
   # 水分提供表
   def self.search_drink(user, drink_type)
-    Guest.joins(:user, :drink).where(users: { id: user }).where(drinks: { drink_type_id: drink_type }).order('first_name_kana  ASC')
+    Guest.joins(:user, :drink).where(users: { id: user }).where(drinks: { drink_type_id: drink_type })
   end
 
   # 水分提供表曜日別
   def self.search_drink_day(user, drink_type, day)
-    Guest.joins(:user, :drink).where(visit1_id: day).or(Guest.joins(:user, :drink).where(visit2_id: day)).where(users: { id: user }).where(drinks: { drink_type_id: drink_type }).order('first_name_kana  ASC')
+    Guest.joins(:user, :drink).where(visit1_id: day).or(Guest.joins(:user, :drink).where(visit2_id: day)).where(users: { id: user }).where(drinks: { drink_type_id: drink_type })
   end
 end
