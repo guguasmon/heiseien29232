09f851c88c27484ce1b915d01a031eacbbafc1a4
guguasmon <hutagoazkey@gmail.com>
2020-10-07 22:47:32 +0900

    uniqueness: { case_sensitive: true }を明示

--- app/models/forbid.rb
+++ app/models/forbid.rb
@@ -2,6 +2,6 @@ class Forbid < ApplicationRecord
   has_many :food_forbid_relations, dependent: :destroy
   has_many :foods, through: :food_forbid_relations
 
-  validates :forbid_food, uniqueness: true
+  validates :forbid_food, uniqueness: { case_sensitive: true }
   validates :forbid_food, presence: true
 end
--- spec/factories/food_forbid_relations.rb
+++ spec/factories/food_forbid_relations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :food_forbid_relation do
-    
+
   end
 end
